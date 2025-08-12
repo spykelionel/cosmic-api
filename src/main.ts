ed1070d5c449dd38d59d80d08c2e117cf8099990
Spyke Lionel <social.lionel@gmail.com>
2025-08-12 14:35:56 +0100

    Update Swagger API endpoint from 'api' to 'swagger' for improved clarity in documentation access.

--- src/main.ts
+++ src/main.ts
@@ -29,7 +29,7 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('swagger', app, document);
 
   const port = process.env.PORT || 3000;
   await app.listen(port);
